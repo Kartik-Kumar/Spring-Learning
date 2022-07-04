f912428b0a0d32d571fc67c0d7c53beb15fb2a35
KartikKumar <kaku16is@gmail.com>
2022-07-04 23:29:06 +0530

    Wiring beans using method Parameter

--- src/main/java/com/example/beans/Person.java
+++ src/main/java/com/example/beans/Person.java
@@ -1,5 +1,7 @@
 package com.example.beans;
 
+import org.springframework.stereotype.Component;
+
 public class Person {
 
     public Person() {
--- src/main/java/com/example/config/ProjectConfig.java
+++ src/main/java/com/example/config/ProjectConfig.java
@@ -2,6 +2,7 @@ package com.example.config;
 
 import com.example.beans.Person;
 import com.example.beans.Vehicle;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -20,11 +21,21 @@ public class ProjectConfig {
         return vehicle;
     }
 
-    @Bean
-    public Person person() {
+//    @Bean(name = "v1")
+//    public Vehicle vehicle1() {
+//        Vehicle vehicle = new Vehicle();
+//        vehicle.setName("auto");
+//        return vehicle;
+//    }
+
+    @Bean    // using parameter
+    public Person person(Vehicle vehicle) {
         Person person = new Person();
         person.setName("Kartik Kumaar");
-        person.setVehicle(vehicle());
+        person.setVehicle(vehicle);
         return person;
     }
 }
+
+
+// Question : what if we have two vechicle bean ? how the context know which one to inject? - try @Qualifier()
