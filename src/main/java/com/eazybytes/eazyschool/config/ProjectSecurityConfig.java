126ed2fd31a7553c9384127322f698ef3c1ba4ca
KartikKumar <kaku16is@gmail.com>
2022-07-17 00:56:16 +0530

    inmemory authentication

--- src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -1,8 +1,12 @@
 package com.eazybytes.eazyschool.config;
 
+import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -10,9 +14,8 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-            http.csrf().disable()
-                .authorizeRequests()
-                .mvcMatchers("/home").permitAll()
+            http.csrf().disable().authorizeRequests()
+                .mvcMatchers("/home").authenticated()
                 .mvcMatchers("/holidays/**").permitAll()
                 .mvcMatchers("/contact").permitAll()
                 .mvcMatchers("/saveMsg").permitAll()
@@ -20,6 +23,15 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/about").permitAll()
                 .and().formLogin().and().httpBasic();
 
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("12345").roles("User")
+                .and()
+                .withUser("kartik").password("KARTIK").roles("User", "Admin")
+                .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
 }
